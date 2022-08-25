9e3652ec60b58e1d2c0d6b177d9649828d642b72
mopanowicz <marek.opanowicz@gmail.com>
2022-08-25 20:49:01 +0200

    Display normalized package names

--- main.py
+++ main.py
@@ -16,20 +16,30 @@ def is_xray_scan(json_data):
 def get_owasp_cves(json_data):
     for dependency in json_data["dependencies"]:
         if "vulnerabilities" in dependency.keys():
-            print(f'{dependency["fileName"]}')
+            for package in dependency["packages"]:
+                pkg_id = package["id"].split("/")
+                pkg_name = pkg_id[1] +":"+ pkg_id[2]
+                print(f'{pkg_name}')
+
+def get_xray_cves(json_data):
+    for vulnerability in json_data["vulnerabilities"]:
+        pkg_name = vulnerability["impactedPackageName"] +"@"+ vulnerability["impactedPackageVersion"]
+        print(f'{pkg_name}')
 
 def get_package_cves(json_data):
     if is_owasp_scan(json_data):
         get_owasp_cves(json_data)
     if is_xray_scan(json_data):
-        print('xray scan')
+        get_xray_cves(json_data)
 
 def get_file_cves(json_file):
     json_data = read_json_file(json_file)
     return get_package_cves(json_data)
 
 def diff_files(json_file_name1, json_file_name2):
+    print(f'\nreport {json_file_name1}\n')
     json_data1 = get_file_cves(json_file_name1)
+    print(f'\nreport {json_file_name2}\n')
     json_data2 = get_file_cves(json_file_name2)
 
 
