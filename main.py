647c0e97c76da788e580a43f887b520fae33d2d4
mopanowicz <marek.opanowicz@gmail.com>
2022-08-26 18:25:22 +0200

    Add diff algorithm + renderer interface

--- main.py
+++ main.py
@@ -2,21 +2,56 @@ import json
 import sys
 
 
-def read_json_file(json_file_name):
+class ReportRenderer:
+    def render_header(self, head1: str, head2: str):
+        pass
+
+    def render_row(self, column1: str, column2: str):
+        pass
+
+    def render_footer(self):
+        pass
+
+
+class MarkDownRenderer(ReportRenderer):
+    def __init__(self, out):
+        self.out = out
+
+    def render_header(self, head1: str, head2: str):
+        self.out.write(f'| {head1} | {head2} |\n')
+        self.out.write('| --- | --- |\n')
+
+    def __column_value(self, column: dict) -> str:
+        column_str = ''
+        if column is not None:
+            column_str += column["name"]
+            if column["vulnerabilities"] and len(column["vulnerabilities"]) > 0:
+                column_str += "<br/>"
+                column_str += "<br/>".join(v["name"] for v in column["vulnerabilities"])
+        return column_str
+
+    def render_row(self, column1: dict, column2: dict):
+        self.out.write(f'| {self.__column_value(column1)} | {self.__column_value(column2)} |\n')
+
+    def render_footer(self):
+        pass
+
+
+def read_json_file(json_file_name: str) -> dict:
     with open(json_file_name, "r") as json_file:
         data = json.load(json_file)
         return data
 
 
-def is_owasp_scan(json_data):
-    return isinstance(json_data, dict) and "projectInfo" in json_data.keys() and "dependencies" in json_data.keys()
+def is_owasp_scan(json_data: dict) -> bool:
+    return "projectInfo" in json_data.keys() and "dependencies" in json_data.keys()
 
 
-def is_xray_scan(json_data):
-    return isinstance(json_data, dict) and "vulnerabilities" in json_data.keys()
+def is_xray_scan(json_data: dict) -> bool:
+    return "vulnerabilities" in json_data.keys()
 
 
-def get_package(packages, pkg_name):
+def get_package(packages: list, pkg_name: str) -> dict:
     for package in packages:
         if package["name"] == pkg_name:
             return package
@@ -30,15 +65,15 @@ def has_vul(pkg_vuls, vul_name):
     return False
 
 
-def get_vul_key(vul):
+def get_vul_key(vul: dict):
     return vul["name"]
 
 
-def get_pkg_key(pkg):
+def get_pkg_key(pkg: dict):
     return pkg["name"]
 
 
-def get_owasp_packages(json_data):
+def get_owasp_packages(json_data: dict):
     packages = []
     for dep in json_data["dependencies"]:
         if "vulnerabilities" in dep.keys():
@@ -58,7 +93,7 @@ def get_owasp_packages(json_data):
     return packages
 
 
-def get_xray_packages(json_data):
+def get_xray_packages(json_data: dict):
     packages = []
     for vul in json_data["vulnerabilities"]:
         package_name = vul["impactedPackageName"] + "@" + vul["impactedPackageVersion"]
@@ -77,27 +112,55 @@ def get_xray_packages(json_data):
     return packages
 
 
-def get_package_cves(json_data):
-    cves = []
+def get_packages(json_data: dict):
+    packages = []
     if is_owasp_scan(json_data):
-        cves = get_owasp_packages(json_data)
+        packages = get_owasp_packages(json_data)
     if is_xray_scan(json_data):
-        cves = get_xray_packages(json_data)
-    return cves
+        packages = get_xray_packages(json_data)
+    return packages
 
 
-def get_file_cves(json_file):
+def get_file_packages(json_file: dict):
     json_data = read_json_file(json_file)
-    return get_package_cves(json_data)
+    return get_packages(json_data)
 
 
-def diff_files(json_file_name1, json_file_name2):
+def diff_files(json_file_name1: str, json_file_name2: str, report_renderer: ReportRenderer):
     print(f'\nreport {json_file_name1}\n')
-    cves1 = get_file_cves(json_file_name1)
-    print(f'{cves1}\n')
+    pkgs1 = get_file_packages(json_file_name1)
+    print(f'{pkgs1}\n')
     print(f'\nreport {json_file_name2}\n')
-    cves2 = get_file_cves(json_file_name2)
-    print(f'{cves2}\n')
+    pkgs2 = get_file_packages(json_file_name2)
+    print(f'{pkgs2}\n')
+
+    report_renderer.render_header(json_file_name1, json_file_name2)
+
+    i1 = 0
+    i2 = 0
+
+    while i1 < len(pkgs1):
+
+        while i2 < len(pkgs2) and pkgs1[i1]["name"] > pkgs2[i2]["name"]:
+            report_renderer.render_row(None, pkgs2[i2])
+            i2 += 1
+
+        column1 = pkgs1[i1]
+        column2 = None
+
+        if i2 < len(pkgs2) and pkgs1[i1]["name"] == pkgs2[i2]["name"]:
+            column2 = pkgs2[i2]
+            i2 += 1
+
+        report_renderer.render_row(column1, column2)
+
+        i1 += 1
+
+    while i2 < len(pkgs2):
+        report_renderer.render_row(None, pkgs2[i2])
+        i2 += 1
+
+    report_renderer.render_footer()
 
 
 if __name__ == '__main__':
@@ -105,4 +168,7 @@ if __name__ == '__main__':
         print(f'Usage:\n{sys.argv[0]} report1.json report2.json')
         sys.exit(1)
 
-    diff_files(sys.argv[1], sys.argv[2])
+    output_file = "cves-diff.md" if len(sys.argv) < 4 else sys.argv[3]
+
+    with open(output_file, 'w') as f:
+        diff_files(sys.argv[1], sys.argv[2], MarkDownRenderer(f))
